769d7669267c67dbd676ffb5a7a4093e2b66048d
Cayo Cesar <cayocesar652@gmail.com>
2024-07-03 11:25:40 -0300

    refactor: Remove unnecessary files and code related to assembler algorithm

--- 2/assembler.py
+++ 2/assembler.py
@@ -3,35 +3,35 @@ def read_data():
         kmers = file.readline().strip().split(',')
     return kmers
 
-def find_best_overlap(genome, kmers):
-    best_overlap_len = 0
+def best_sobreposition(genome, kmers):
+    best_sobreposition_len = 0
     best_kmer_index = -1
     k = len(kmers[0])
     
     for i, kmer in enumerate(kmers):
         for j in range(k - 1, 0, -1):
             if genome.endswith(kmer[:j]):
-                if j > best_overlap_len:
-                    best_overlap_len = j
+                if j > best_sobreposition_len:
+                    best_sobreposition_len = j
                     best_kmer_index = i
                 break
             elif genome.startswith(kmer[-j:]):
-                if j > best_overlap_len:
-                    best_overlap_len = j
+                if j > best_sobreposition_len:
+                    best_sobreposition_len = j
                     best_kmer_index = i
                 break
 
-    return best_overlap_len, best_kmer_index
+    return best_sobreposition_len, best_kmer_index
 
 def assembler(kmers):
     genome = kmers.pop(0)
     while kmers:
-        best_overlap_len, best_kmer_index = find_best_overlap(genome, kmers)
-        if best_overlap_len > 0:
-            if genome.endswith(kmers[best_kmer_index][:best_overlap_len]):
-                genome += kmers[best_kmer_index][best_overlap_len:]
+        best_sobreposition_len, best_kmer_index = best_sobreposition(genome, kmers)
+        if best_sobreposition_len > 0:
+            if genome.endswith(kmers[best_kmer_index][:best_sobreposition_len]):
+                genome += kmers[best_kmer_index][best_sobreposition_len:]
             else:
-                genome = kmers[best_kmer_index][:-best_overlap_len] + genome
+                genome = kmers[best_kmer_index][:-best_sobreposition_len] + genome
         else:
             genome += kmers[best_kmer_index]
         kmers.pop(best_kmer_index)

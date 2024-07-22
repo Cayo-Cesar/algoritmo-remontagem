45d3589e9184465d3e5006c9a3faac963ca4ad7e
Cayo Cesar <cayocesar652@gmail.com>
2024-07-22 14:55:39 -0300

    refactor: Update DNAAssembler class to improve genome assembly algorithm

--- src/assembler.py
+++ src/assembler.py
@@ -5,21 +5,19 @@ class DNAAssembler:
         self.kmers = []
         self.genome = ""
 
-    # Read data from input file
     def read_data(self):
         with open(self.input_file, 'r') as file:
             self.kmers = file.readline().strip().split(',')
 
-    # Find the best sobreposition between the genome and the kmers
     def best_sobreposition(self, genome, kmers):
         best_sobreposition_len = 0
         best_kmer_index = -1
         k = len(kmers[0])
-        
+
         for i, kmer in enumerate(kmers):
-            for j in range(k - 1, 0, -1):
+            for j in range(k-1, 0, -1):
                 if genome.endswith(kmer[:j]):
-                    if j > best_sobreposition_len:
+                    if j>best_sobreposition_len:
                         best_sobreposition_len = j
                         best_kmer_index = i
                     break
@@ -31,15 +29,14 @@ class DNAAssembler:
 
         return best_sobreposition_len, best_kmer_index
     
-    # Assemble the genome from the kmers
     def assembler(self):
         self.genome = self.kmers.pop(0)
         while self.kmers:
             best_sobreposition_len, best_kmer_index = self.best_sobreposition(self.genome, self.kmers)
             print(f"Best sobreposition length: {best_sobreposition_len}, Best kmer index: {best_kmer_index}")
-            
+
             if best_kmer_index == -1:
-                print("No valid k-mer found, appending the next k-mer.")
+                print("No valid kmer found")
                 self.genome += self.kmers.pop(0)
             else:
                 if best_sobreposition_len > 0:
@@ -49,14 +46,13 @@ class DNAAssembler:
                         self.genome = self.kmers[best_kmer_index][:-best_sobreposition_len] + self.genome
                 else:
                     self.genome += self.kmers[best_kmer_index]
+
                 self.kmers.pop(best_kmer_index)
 
-    # Write the genome to output file
     def write_data(self):
         with open(self.output_file, 'w') as file:
             file.write(self.genome)
 
-    # Run the assembler
     def run(self):
         self.read_data()
         self.assembler()

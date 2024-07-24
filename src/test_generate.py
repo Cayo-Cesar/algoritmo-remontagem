0c6773c0325c2a1ebc7f933ef838f8140d2cd688
Cayo Cesar <cayocesar652@gmail.com>
2024-07-24 16:32:52 -0300

    refactor: Update DNAAssembler class to optimize genome assembly algorithm and improve code readability

--- src/test_generate.py
+++ src/test_generate.py
@@ -4,7 +4,7 @@ def generate_dna_sequence(length):
     bases = ['A', 'T', 'C', 'G']
     return ''.join(random.choices(bases, k=length))
 
-dna_sequence = generate_dna_sequence(500000)
+dna_sequence = generate_dna_sequence(700000)
 
 with open("dna_sequence.txt", "w") as file:
     file.write(dna_sequence)

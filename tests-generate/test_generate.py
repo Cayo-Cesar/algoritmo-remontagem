761397324395055e4b9e069adc281a26befe304a
Cayo Cesar <cayocesar652@gmail.com>
2024-07-30 13:23:26 -0300

    Final commit

--- tests-generate/test_generate.py
+++ tests-generate/test_generate.py
@@ -4,9 +4,9 @@ def generate_dna_sequence(length):
     bases = ['A', 'T', 'C', 'G']
     return ''.join(random.choices(bases, k=length))
 
-dna_sequence = generate_dna_sequence(700000)
+dna_sequence = generate_dna_sequence(1000000)
 
-with open("dna_sequence.txt", "w") as file:
+with open("tests-generate/dna_sequence.txt", "w") as file:
     file.write(dna_sequence)
 
 print("Sequência de DNA gerada e salva em 'dna_sequence.txt'")
